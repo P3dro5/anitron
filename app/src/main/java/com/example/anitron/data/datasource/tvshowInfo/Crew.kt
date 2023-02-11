7f1a7d34258d8b25fafa33058e847ee46d217214
P3dro5 <pedro.santos.rafael@fmail.com>
2023-02-11 01:01:07 +0000

    Problem fixed for some people information not displaying due to class data not being correct.

--- app/src/main/java/com/example/anitron/data/datasource/tvshowInfo/Crew.kt
+++ app/src/main/java/com/example/anitron/data/datasource/tvshowInfo/Crew.kt
@@ -13,7 +13,7 @@ data class Crew (
     @SerializedName("popularity") var popularity : String?,
     @SerializedName("genre_ids") var genreIds : List<String>,
     @SerializedName("cast") var cast : List<String>,
-    @SerializedName("vote_average") var voteAverage : String,
-    @SerializedName("release_date") var releaseDate : String,
-    @SerializedName("credit_id") var creditId : List<String>,
+    @SerializedName("vote_average") var voteAverage : String?,
+    @SerializedName("release_date") var releaseDate : String?,
+    @SerializedName("credit_id") var creditId : String?,
 )
